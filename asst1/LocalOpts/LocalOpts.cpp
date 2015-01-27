f9ab6edaed190ac4c59535748ee30d052fb45d2a
Michael Choquette <mchoquet1@gmail.com>
2015-01-26 20:00:23 -0500

    big refactor of LocalOpts

--- asst1/LocalOpts/LocalOpts.cpp
+++ asst1/LocalOpts/LocalOpts.cpp
@@ -19,8 +19,145 @@ using namespace llvm;
 namespace {
 
 class LocalOpts : public ModulePass {
+ private:
+  
+  /*
+    If the input binary operator instruction can be constant folded to a number, do so and
+    return the result. Otherwise, return null.
+  */
+  ConstantInt *tryConstantFold(BinaryOperator *inst) {
+    ConstantInt *lPtr = dyn_cast<ConstantInt>(inst->getOperand(0));
+    ConstantInt *rPtr = dyn_cast<ConstantInt>(inst->getOperand(1));
+    LLVMContext& ctx = inst->getContext();
+    if (lPtr && rPtr) {
+      const APInt& l = lPtr->getValue();
+      const APInt& r = rPtr->getValue();
+      switch (inst->getOpcode()) {
+        case (Instruction::Add):
+          return ConstantInt::get(ctx, l + r);
+        case (Instruction::Sub):
+          return ConstantInt::get(ctx, l - r);
+        case (Instruction::Mul):
+          return ConstantInt::get(ctx, l * r);
+        case (Instruction::SDiv):
+          return ConstantInt::get(ctx, l.sdiv(r));
+        case (Instruction::UDiv):
+          return ConstantInt::get(ctx, l.udiv(r));
+        default:
+          return nullptr;
+      }
+    } else {
+      return nullptr;
+    }
+  }
 
-  // do some local optimizations on the module
+  /* If the operator is commutative, orders the operands such that constants prefer to be in
+   * arg2 and non-constants prefer arg1. This reduces the number of cases we have to consider
+   * in the other functions.
+   * If the operator is non-commutative, it does nothing. */
+  void orderOperands(const unsigned opCode, Value **arg1, Value **arg2) {
+    if ((opCode == Instruction::Add || opCode == Instruction::Mul) &&
+        isa<ConstantInt>(*arg1)) {
+      std::swap(*arg1, *arg2);
+    }
+  }
+
+  /*
+    If the input binary operator instruction can be simplified using an algebraic identity
+    (i.e. x + 0 == x,  y - y == 0), return the resulting value.
+    NOTE: the handout for this hw mentions that we don't have to worry about divide-by-zero
+          issues.
+
+    Example cases:
+    x + 0 == x
+    x - 0 == x
+    x - x == 0
+    x * 0 == 0
+    x * 1 == x
+    x / x == 1
+    x / 1 == x
+    0 / x == 0 
+  */
+  Value *tryAlgebraicIdentity(BinaryOperator *inst) {
+    const unsigned opCode = inst->getOpcode();
+    Value *l = inst->getOperand(0);
+    Value *r = inst->getOperand(1);
+    orderOperands(opCode, &l, &r);
+    ConstantInt *lAsInt = dyn_cast<ConstantInt>(l);
+    ConstantInt *rAsInt = dyn_cast<ConstantInt>(r);
+    switch (opCode) {
+      case (Instruction::Add):
+        if (rAsInt && rAsInt->isZero()) {
+          return l;
+        }
+        break;
+      case (Instruction::Sub):
+        if (rAsInt && rAsInt->isZero()) {
+          return l;
+        } else if (l == r) {
+          return ConstantInt::get(l->getType(), 0);
+        }
+        break;
+      case (Instruction::Mul):
+        if (rAsInt && rAsInt->isZero()) {
+          return rAsInt;
+        } else if (rAsInt && rAsInt->isOne()) {
+          return l;
+        }
+        break;
+      case (Instruction::UDiv): case (Instruction::SDiv):
+        if (l == r) {
+          return ConstantInt::get(l->getType(), 1);
+        } else if (rAsInt && rAsInt->isOne()) {
+          return l;
+        } else if (lAsInt && lAsInt->isZero()) {
+          return lAsInt;
+        }
+        break;
+      default:
+        return nullptr;
+    }
+    return nullptr;
+  }
+
+  /*
+    Tries to apply a local strength reduction to simplify the given binop instruction.
+
+    Example cases:
+    x * (2 ^ k) == x << k
+    x / (2 ^ k) == x >> k
+  */
+  BinaryOperator *tryStrengthReduction(BinaryOperator *inst) {
+    const unsigned opCode = inst->getOpcode();
+    Value *l = inst->getOperand(0);
+    Value *r = inst->getOperand(1);
+    orderOperands(opCode, &l, &r);
+    ConstantInt *lAsInt = dyn_cast<ConstantInt>(l);
+    ConstantInt *rAsInt = dyn_cast<ConstantInt>(r);
+    switch (opCode) {
+      case (Instruction::Mul):
+        if (rAsInt && rAsInt->getValue().isPowerOf2()) {
+          uint64_t k = rAsInt->getValue().countTrailingZeros();
+          return BinaryOperator::Create(Instruction::Shl, l, ConstantInt::get(l->getType(), k));
+        }
+        break;
+      case (Instruction::UDiv):
+        if (rAsInt && rAsInt->getValue().isPowerOf2()) {
+          uint64_t k = rAsInt->getValue().countTrailingZeros();
+          return BinaryOperator::Create(Instruction::LShr, l, ConstantInt::get(l->getType(), k));
+        }
+        break;
+      case (Instruction::SDiv):
+        if (rAsInt && rAsInt->getValue().isPowerOf2()) {
+          uint64_t k = rAsInt->getValue().countTrailingZeros();
+          return BinaryOperator::Create(Instruction::AShr, l, ConstantInt::get(l->getType(), k));
+        }
+        break;
+      default:
+        return nullptr;
+    }
+    return nullptr;
+  }
 
   void doLocalOpts(Module& M) {
     outs() << "Module " << M.getModuleIdentifier().c_str() << "\n";
@@ -29,119 +166,40 @@ class LocalOpts : public ModulePass {
     int nConstFold = 0;
     int nStrRed = 0;
 
-    bool progress = true;
-
-    while(progress)
-    {
-      int prevAlgIdent = nAlgIdent;
-      int prevConstFold = nConstFold;
-      int prevStrRed = nStrRed;
-
-      for(Module::iterator miter = M.begin(); miter != M.end(); ++miter)
-      {
-        for(Function::iterator fiter = miter->begin(); fiter != miter->end(); ++fiter)
-        {
-          for(BasicBlock::iterator biter = fiter->begin(); biter != fiter->end(); ++biter)
-          {
-            if(BinaryOperator* inst = dyn_cast<BinaryOperator>(&*biter))
-            {
-              inst->print(errs()); outs() << "\n";
-		ConstantInt* x = dyn_cast<ConstantInt>(inst->getOperand(0));
-              ConstantInt* y = dyn_cast<ConstantInt>(inst->getOperand(1));
-
-              if(x && y)
-              {
-                //here we can constant fold
-                const APInt& xval = x->getValue();
-                const APInt& yval = y->getValue();
-                ConstantInt* result = 0;
-
-                // 3 types of constant folding (addition, subtraction, multiplication)
-                if(inst->getOpcode() == Instruction::Add)
-                  result = ConstantInt::get(x->getContext(), xval+yval);
-                else if(inst->getOpcode() == Instruction::Sub)
-                  result = ConstantInt::get(x->getContext(), xval-yval);
-                else if(inst->getOpcode() == Instruction::Mul)
-                  result = ConstantInt::get(x->getContext(), xval*yval);
-
-                if(result)
-                {
-                  ReplaceInstWithValue(inst->getParent()->getInstList(), biter, result);
-                  nConstFold++;
-                }
-              }
-
-              else if (x || y)
-              {
-                ConstantInt* constval;
-                Value* constval_v;
-                Value* other_v;
-                if(x)
-                {
-                  constval = x;
-                  constval_v = inst->getOperand(0);
-                  other_v = inst->getOperand(1);
-                }
-                else if(y)
-                {
-                  constval = y;
-                  constval_v = inst->getOperand(1);
-                  other_v = inst->getOperand(0);
-                }
-                // algebraic identity or strength reduction might be doable
-
-                if(constval->isZero())
-                {
-                  if(inst->getOpcode() == Instruction::Add)
-                  {             
-                       // algebraic identity 1: x + 0 = x
-                       ReplaceInstWithValue(inst->getParent()->getInstList(), biter, other_v);
-                       nAlgIdent++; 
-                  }
-                  else if(inst->getOpcode() == Instruction::Mul)
-                  {
-                        //algebraic identity 2: x * 0 = 0
-                        ReplaceInstWithValue(inst->getParent()->getInstList(), biter, constval_v);
-                        nAlgIdent++;
-                  }
-                }
-
-                if(inst->getOpcode() == Instruction::Mul)
-                {
-			//strength reduction 1: x * powerof2 = x shl log2(powerof2)
-                  if(constval->getValue().isPowerOf2())
-                  {
-                    Value* nshift_v = ConstantInt::get(constval->getType(), log2(constval->getSExtValue()));
-                    ReplaceInstWithInst(inst->getParent()->getInstList(), biter, BinaryOperator::Create(Instruction::Shl, other_v, nshift_v));
-			nStrRed++;
-                  }
-                } 
-
-                if(y && (inst->getOpcode() == Instruction::UDiv || inst->getOpcode() == Instruction::SDiv))
-                {
-			//strength reduction 2: x / powerof2 = x lshr log2(powerof2)
-                    if(constval->getValue().isPowerOf2())
-                    {
-                      Value* nshift_v = ConstantInt::get(constval->getType(), log2(constval->getSExtValue()));
-                      ReplaceInstWithInst(inst->getParent()->getInstList(), biter, BinaryOperator::Create(Instruction::LShr, other_v, nshift_v));
-			nStrRed++;
-                    }
-                }
-              }
-              
+    for(Function& F : M) {
+      for(BasicBlock& B : F) {
+        for(BasicBlock::iterator biter = B.begin(), bEnd = B.end(); biter != bEnd;) {
+          if(BinaryOperator* inst = dyn_cast<BinaryOperator>(&*biter)) {
+            inst->print(errs()); outs() << "\n";
+            if (ConstantInt *result = tryConstantFold(inst)) {
+              outs() << "replaced with " << result->getValue() << "\n";
+              ReplaceInstWithValue(inst->getParent()->getInstList(), biter, result);
+              nConstFold++;
+            } else if (Value *result = tryAlgebraicIdentity(inst)) {
+              outs() << "replaced with ";
+              result->printAsOperand(outs());
+              outs() << "\n";
+              ReplaceInstWithValue(inst->getParent()->getInstList(), biter, result);
+              nAlgIdent++;
+            } else if (BinaryOperator *result = tryStrengthReduction(inst)) {
+              outs() << "replaced with ";
+              result->print(outs());
+              outs() << "\n";
+              ReplaceInstWithInst(inst->getParent()->getInstList(), biter, result);
+              nStrRed++;
+              ++biter;
+            } else {
+              ++biter;
             }
+          } else {
+            ++biter;
           }
-
-
         }
       }
-
-      progress = (nAlgIdent != prevAlgIdent) || (nStrRed != prevStrRed) || (nConstFold != prevConstFold);
-	outs() << "\n\n";
     }
-
-    outs() << "Transformations applied:" << "\n" << "\tAlgebraic identities: " << nAlgIdent << "\n" << "\tConstant folding: " << nConstFold << "\n" << "\tStrength reduction: " << nStrRed << "\n";
-
+    outs() << "Transformations applied:\n\tAlgebraic identities: " << nAlgIdent
+           << "\n\tConstant folding: " << nConstFold
+           << "\n\tStrength reduction: " << nStrRed << "\n";
   }
 
 public:
